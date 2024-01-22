05569e035d7f35a0c4b105be0bfb4e14b510d1a7
DivineSoulXVI <riquentel.arrindell@gmail.com>
2024-01-22 05:43:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// for the navbar
+    function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+} else {
+    x.className = "topnav";
+}
+}
+
